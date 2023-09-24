0bf81e450e44e8ef50c6a3e6b2fcf3625723219b
Muhammad Khaliq Teuku Ansari <2110817310008@mhs.ulm.ac.id>
2023-09-24 13:27:30 +0800

    style: styling kampus page

--- resources/views/page/kampus.blade.php
+++ resources/views/page/kampus.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profil Mahasiswa</title>
+    <title>Profil Kampus</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         /* CSS untuk mengatur tata letak konten di tengah */
@@ -37,36 +37,21 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
-                    <h1>Halaman Profil</h1>
+                    <h1>Halaman Informasi Kampus</h1>
                     <div class="card">
                         <div>
                             <img src="{{ asset('asset/ulm.png') }}" alt="Foto Profil" width="200px" height="200px">
                         </div>
                         <div class="card-body">
-                            <table class="profile-table">
-                                <tr>
-                                    <td>
-                                        <h5>Nama :</h5>
-                                    </td>
-                                    <td>
-                                        <h6>{{ $univ }}</h6>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <h5>Alamat:</h5>
-                                    </td>
-                                    <td>
-                                        <h6>{{ $alamat }}</h6>
-                                    </td>
-                                </tr>
-                            </table>
+                            <h3>{{$univ}}</h3><br>
+                            <h4>Alamat</h4>
+                            <h5>{{$alamat}}</h5>
                         </div>
                     </div>
             </div>
         </div>
-        <a href="/" class="btn btn-danger mt-3">Kembali</a>
-        <a href="/kampus" class="btn btn-primary mt-3">Kampus</a>
+        <a href="/" class="btn btn-danger mt-3">Beranda</a>
+        <a href="/profile" class="btn btn-primary mt-3">Profil</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
--- resources/views/page/profile.blade.php
+++ resources/views/page/profile.blade.php
@@ -83,7 +83,7 @@
                     </div>
             </div>
         </div>
-        <a href="/" class="btn btn-danger mt-3">Kembali</a>
+        <a href="/" class="btn btn-danger mt-3">Beranda</a>
         <a href="/kampus" class="btn btn-primary mt-3">Kampus</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
